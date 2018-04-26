2e58d5b0c18601c3b310c70be18fa58250c6b658
ColleenM <crawleen@yahoo.com>
2018-04-25 20:09:04 -0600

    functioning thumbs up and down with database and react

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,18 +2,19 @@ import axios from "axios";
 
 export default {
     getBooks: function() {
-        console.log("HERE");
-        return axios.get("/api/restaurants");
-      },
-      getBook: function(id) {
-        return axios.get("/api/restaurants/" + id);
-      },
-      getRestaurants: function(liked) {
-        console.log(liked);
-        return axios.get("/api/restaurants/:" +liked);
-      },
+      return axios.get("/api/restaurants");
+    },
+    updateLike: function(id) {
+      return axios.put("/api/restaurants/thumbsup" + id);
+    },
+    updateDislike: function(id) {
+      return axios.put("/api/restaurants/thumbsdown" + id);
+    },
+    getRestaurants: function(liked) {
+      return axios.get("/api/restaurants/:" +liked);
+    },
     saveRestaurants: function(restaurantData) {
-        return axios.post("/api/restaurants", restaurantData);
+      return axios.post("/api/restaurants", restaurantData);
     }
     
 };
